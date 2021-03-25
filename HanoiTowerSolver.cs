8a8f813f8f16067f20bd28775d48980f8d0111b7
HAROS <harchenk0s@yandex.ru>
2021-03-26 03:34:10 +0700

    Fix bugs, Algorithm is working

--- HanoiTowerSolver.cs
+++ HanoiTowerSolver.cs
@@ -19,12 +19,18 @@ namespace Algoritmes
             towers.Add(new List<int>());
             towers.Add(new List<int>());
 
-            for (int i = 1; i <= TowerHeight; i++)
+            for (int i = 1; i <= TowerHeight ; i++)
             {
                 towers[0].Add(i);
                 towers[1].Add(0);
                 towers[2].Add(0);
             }
+
+            foreach (var item in towers)
+            {
+                item.Reverse();
+            }
+
         }
 
 
@@ -57,13 +63,13 @@ namespace Algoritmes
 
         class EvenNumberAlg : IStrategy
         {
-            List<List<int>> towers;
+            List<List<int>> list;
             public int CountMinSteps;
             public int Count = 0;
 
             public EvenNumberAlg(List<List<int>> tow)
             {
-                towers = tow;
+                list = tow;
                 CountMinSteps = Convert.ToInt32(Math.Pow(2, tow[0].Count) - 1);
             }
 
@@ -75,7 +81,7 @@ namespace Algoritmes
                     {
                         Swap(0, 1);
                         Count++;
-                        Process();
+                        Process(Count, CountMinSteps);
                     }
                     else
                     {
@@ -86,7 +92,7 @@ namespace Algoritmes
                     {
                         Swap(0, 2);
                         Count++;
-                        Process();
+                        Process(Count, CountMinSteps);
                     }
                     else
                     {
@@ -97,7 +103,7 @@ namespace Algoritmes
                     {
                         Swap(1, 2);
                         Count++;
-                        Process();
+                        Process(Count, CountMinSteps);
                     }
                     else
                     {
@@ -111,55 +117,71 @@ namespace Algoritmes
                 int A = 0;
                 int B = 0;
 
-                for (int i = towers[posA].Count-1; i > 0; i--)
+                for (int i = 0; i < list[posA].Count; i++)
                 {
-                    if(towers[posA][i] != 0)
+                    if (list[posA][i] != 0)
                     {
-                        A = towers[posA][i];
-                        break;
+                        A = list[posA][i];
                     }
                 }
+                A = list[posA].IndexOf(A);
 
-                for (int i = towers[posB].Count-1; i > 0; i--)
+                for (int i = 0; i < list[posB].Count; i++)
                 {
-                    if (towers[posB][i] != 0)
+                    if (list[posB][i] != 0)
                     {
-                        B = towers[posB][i];
-                        break;
+                        B = list[posB][i];
                     }
                 }
+                B = list[posB].IndexOf(B);
 
-                if(A != B)
+
+                if (list[posA][A] > list[posB][B])
                 {
-                    if (A > B)
+                    if(list[posB][B] == 0)
                     {
-                        towers[posB].Add(A);
+                        list[posB][B] = list[posA][A];
+                        list[posA][A] = 0;
                     }
-
-                    if (A < B)
+                    else
                     {
-                        towers[posA].Add(B);
+                        list[posA][A + 1] = list[posB][B];
+                        list[posB][B] = 0;
+                    }
+                }
+                else if(list[posA][A] < list[posB][B])
+                {
+                    if(list[posA][A] == 0)
+                    {
+                        list[posA][A] = list[posB][B];
+                        list[posB][B] = 0;
+                    }
+                    else
+                    {
+                        list[posB][B + 1] = list[posA][A];
+                        list[posA][A] = 0;
                     }
                 }
             }
 
-            void Process()
+            void Process(int count, int max)
             {
                 Console.ReadKey(false);
-                Display.RefreshDisplay(towers);
+                Display.RefreshDisplay(list);
+                Console.WriteLine($"Steps: {max}\nStep:{count}");
             }
         }
 
 
         class OddNumberAlg : IStrategy
         {
-            List<List<int>> towers;
+            List<List<int>> list;
             public int CountMinSteps;
             public int Count = 0;
 
             public OddNumberAlg(List<List<int>> tow)
             {
-                towers = tow;
+                list = tow;
                 CountMinSteps = Convert.ToInt32(Math.Pow(2, tow[0].Count) - 1);
             }
 
@@ -171,7 +193,7 @@ namespace Algoritmes
                     {
                         Swap(0, 2);
                         Count++;
-                        Process();
+                        Process(Count, CountMinSteps);
                     }
                     else
                     {
@@ -182,7 +204,7 @@ namespace Algoritmes
                     {
                         Swap(0, 1);
                         Count++;
-                        Process();
+                        Process(Count, CountMinSteps);
                     }
                     else
                     {
@@ -193,7 +215,7 @@ namespace Algoritmes
                     {
                         Swap(1, 2);
                         Count++;
-                        Process();
+                        Process(Count, CountMinSteps);
                     }
                     else
                     {
@@ -207,46 +229,58 @@ namespace Algoritmes
                 int A = 0;
                 int B = 0;
 
-                for (int i = 0; i < towers[posA].Count; i++)
+                for (int i = 0; i < list[posA].Count; i++)
                 {
-                    if (towers[posA][i] != 0)
+                    if (list[posA][i] != 0)
                     {
-                        A = towers[posA][i];
-                        break;
+                        A = list[posA][i];
                     }
-                    A = towers[posA].Count;
                 }
+                A = list[posA].IndexOf(A);
 
-                for (int i = 0; i < towers[posB].Count; i++)
+                for (int i = 0; i < list[posB].Count; i++)
                 {
-                    if (towers[posB][i] != 0)
+                    if (list[posB][i] != 0)
                     {
-                        B = towers[posB][i];
-                        break;
+                        B = list[posB][i];
                     }
-                    B = towers[posB].Count;
                 }
+                B = list[posB].IndexOf(B);
 
-                if (A != B)
+
+                if (list[posA][A] > list[posB][B])
                 {
-                    if (A > B)
+                    if (list[posB][B] == 0)
                     {
-                        towers[posB].Insert(0, A);
-                        towers[posA][A] = 0;
+                        list[posB][B] = list[posA][A];
+                        list[posA][A] = 0;
                     }
-
-                    if (A < B)
+                    else
                     {
-                        towers[posA].Insert(0, B);
-                        towers[posB][B] = 0;
+                        list[posA][A + 1] = list[posB][B];
+                        list[posB][B] = 0;
+                    }
+                }
+                else if (list[posA][A] < list[posB][B])
+                {
+                    if (list[posA][A] == 0)
+                    {
+                        list[posA][A] = list[posB][B];
+                        list[posB][B] = 0;
+                    }
+                    else
+                    {
+                        list[posB][B + 1] = list[posA][A];
+                        list[posA][A] = 0;
                     }
                 }
             }
 
-            void Process()
+            void Process(int count, int max)
             {
                 Console.ReadKey(false);
-                Display.RefreshDisplay(towers);
+                Display.RefreshDisplay(list);
+                Console.WriteLine($"Steps: {max}\nStep:{count}");
             }
         }
 
@@ -255,49 +289,33 @@ namespace Algoritmes
         {
             public static void RefreshDisplay(List<List<int>> list)
             {
+                List<List<int>> towers = new List<List<int>>();
                 Console.Clear();
-                for (int i = 0; i < list[0].Count + 25; i++)
-                {
-                    Console.Write("=");
-                }
+                towers.Add(new List<int>());
+                towers.Add(new List<int>());
+                towers.Add(new List<int>());
+                Console.WriteLine("===================");
 
-                Console.WriteLine("\n");
-
-                for (int i = list[0].Count; i > 0 ; i--)
+                for (int i = list[0].Count - 1; i >= 0 ; i--)
                 {
-                    for (int k = 0; k < 3; k++)
+                    for (int j = 0; j < 3; j++)
                     {
-                        Console.Write("  ");
-                        for (int j = 0; j < list[k].Count / 2; j++)
+                        if(list[j][i] == 0)
                         {
-                            Console.Write(" ");
-                        }
-
-
-                        if (list[k][list[k].Count - i] == 0)
-                        {
-                            Console.Write("|");
+                            Console.Write("|  ");
                         }
                         else
                         {
-                            Console.Write(list[k][list[k].Count - i]);
+                            Console.Write($"{list[j][i]}  ");
                         }
-
-
-                        for (int j = 0; j < list[k].Count / 2; j++)
-                        {
-                            Console.Write(" ");
-                        }
-                        Console.Write("  ");
+                        
                     }
-                    Console.Write("\n");
-
+                    Console.WriteLine();
                 }
+                
 
-                for (int i = 0; i < list[0].Count + 25; i++)
-                {
-                    Console.Write("_");
-                }
+                Console.WriteLine("\n===================");
+
             }
         }
     }
